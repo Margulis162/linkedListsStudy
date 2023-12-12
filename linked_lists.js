e38db74fe339a52719905a9d4462fd7bac37b885
Margulis162 <markmargulis@proton.me>
2023-12-12 08:38:58 -0500

    insert last function

--- linked_lists.js
+++ linked_lists.js
@@ -1,11 +1,72 @@
-const n1 ={
-    data: 100
-};
+// minimalistic example of linked lists
+// const n1 ={
+//     data: 100
+// };
 
-const n2 = {
-    data:100500
+// const n2 = {
+//     data:100500
+// }
+
+// n1.next = n2;
+
+// console.log(n1);
+
+class Node{
+    constructor(data, next = null){
+        this.data = data;
+        this.next = next;
+    }
 }
 
-n1.next = n2;
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+    //insert first node 
+    insertFirst(data){
+        this.head = new Node(data, this.head);
+        this.size++;
+    }
 
-console.log(n1);
+    //insert last node 
+    insertLast(data){
+        let node = new Node(data);
+        let current;
+        //If empty, make head
+    if(!this.head){
+        this.head = node;
+    }else{
+        current = this.head;
+        while(current.next){
+            current = current.next;
+        }
+        current.next = node; 
+    }
+    this.size++;
+    }
+
+    
+    //insert at index
+    
+    //remove at index
+
+    //clear
+
+    //print
+    printListData(){
+        let current = this.head;
+
+        while(current){
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+const ll = new LinkedList();
+ll.insertFirst(100);
+ll.insertFirst(300);
+ll.insertFirst(340);
+ll.insertLast(400); 
+ll.printListData();
